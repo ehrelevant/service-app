e930d534e418cac9cc3973d759d8d6ce3911dfa6
Ehren Castillo <ehrencastillo@gmail.com>
2025-10-02 17:24:47 +0800

    feat(eslint): setup linting with eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+import expo from 'eslint-plugin-expo';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import imsort from '@bastidood/eslint-plugin-imsort';
+import turbo from "eslint-plugin-turbo";
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  { ignores: ['node_modules/**/*', 'build/**/*', 'dist/**/*'] },
+  { languageOptions: { globals: { ...globals.node }  }},
+  {
+    files: ['**/*.{js,jsx,cjs,mjs,ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...ts.configs.recommended,
+      ...ts.configs.stylistic,
+      imsort.configs.all,
+      prettier,
+    ],
+    plugins: { '@bastidood/imsort': imsort },
+  },
+  {
+    files: ['**/*.jsx', '**/*.tsx'],
+    plugins: {
+      expo,
+      react,
+      'react-hooks': reactHooks
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'expo/use-dom-exports': 'error',
+      'expo/no-env-var-destructuring': 'error',
+      'expo/no-dynamic-env-var': 'error',
+      'react/no-unknown-property': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/no-this-in-sfc': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      }
+    }
+  },
+  {
+    plugins: {
+      turbo,
+    },
+    rules: {
+      "turbo/no-undeclared-env-vars": "warn",
+    },
+  }
+]);
