89933eb758d956f897d47625fbadec2cf6099607
Ehren Castillo <ehrencastillo@gmail.com>
2025-10-11 17:57:51 +0800

    refactor(packages/database): prefer snake_case for table names

--- packages/database/src/schema/app.ts
+++ packages/database/src/schema/app.ts
@@ -6,17 +6,17 @@ export const app = pgSchema('app');
 export const user = app.table(
   'user',
   {
-    id: uuid().primaryKey().notNull().defaultRandom(),
-    email: text().unique().notNull(),
-    firstName: text().notNull().default(''),
-    middleName: text().notNull().default(''),
-    lastName: text().notNull().default(''),
-    phoneNumber: text().unique().notNull(),
-    birthDate: date({ mode: 'date' }).notNull(),
-    image: text(),
-    emailVerified: boolean().notNull().default(false),
-    createdAt: timestamp({ mode: 'date', withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ mode: 'date', withTimezone: true })
+    id: uuid('id').primaryKey().notNull().defaultRandom(),
+    email: text('email').unique().notNull(),
+    firstName: text('first_name').notNull().default(''),
+    middleName: text('middle_name').notNull().default(''),
+    lastName: text('last_name').notNull().default(''),
+    phoneNumber: text('phone_number').unique().notNull(),
+    birthDate: date('birth_date', { mode: 'date' }).notNull(),
+    image: text('image'),
+    emailVerified: boolean('email_verified').notNull().default(false),
+    createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).notNull().defaultNow(),
+    updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true })
       .notNull()
       .defaultNow()
       .$onUpdate(() => new Date()),
@@ -30,8 +30,8 @@ export type User = typeof user.$inferSelect;
 export type NewUser = typeof user.$inferInsert;
 
 export const role = app.table('role', {
-  id: uuid().primaryKey().notNull().defaultRandom(),
-  name: text().notNull().unique(),
+  id: uuid('id').primaryKey().notNull().defaultRandom(),
+  name: text('name').notNull().unique(),
 });
 export type Role = typeof role.$inferSelect;
 export type NewRole = typeof role.$inferInsert;
@@ -39,13 +39,13 @@ export type NewRole = typeof role.$inferInsert;
 export const userRole = app.table(
   'user_role',
   {
-    userId: uuid()
+    userId: uuid('user_id')
       .notNull()
       .references(() => user.id, { onUpdate: 'cascade' }),
-    roleId: uuid()
+    roleId: uuid('role_id')
       .notNull()
       .references(() => role.id, { onUpdate: 'cascade' }),
-    assigned_at: timestamp({ mode: 'date', withTimezone: true }).defaultNow(),
+    assigned_at: timestamp('assigned_at', { mode: 'date', withTimezone: true }).defaultNow(),
   },
   ({ userId, roleId }) => [primaryKey({ columns: [userId, roleId] })],
 );
--- packages/database/src/schema/auth.ts
+++ packages/database/src/schema/auth.ts
@@ -5,45 +5,45 @@ import { user } from './app';
 export const auth = pgSchema('auth');
 
 export const session = auth.table('session', {
-  id: uuid().primaryKey().notNull().defaultRandom(),
-  userId: uuid()
+  id: uuid('id').primaryKey().notNull().defaultRandom(),
+  userId: uuid('user_id')
     .notNull()
     .references(() => user.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
-  token: text().notNull().unique(),
+  token: text('token').notNull().unique(),
   expiresAt: timestamp({ mode: 'date', withTimezone: true }).notNull(),
-  ipAddress: text(),
-  userAgent: text(),
-  createdAt: timestamp({ mode: 'date', withTimezone: true }).defaultNow().notNull(),
-  updatedAt: timestamp({ mode: 'date', withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
+  ipAddress: text('ip_address'),
+  userAgent: text('user_agent'),
+  createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
 });
 export type Session = typeof session.$inferSelect;
 export type NewSession = typeof session.$inferInsert;
 
 export const account = auth.table('account', {
-  id: uuid().primaryKey().notNull().defaultRandom(),
-  userId: uuid()
+  id: uuid('id').primaryKey().notNull().defaultRandom(),
+  userId: uuid('user_id')
     .notNull()
     .references(() => user.id, { onUpdate: 'cascade', onDelete: 'cascade' }),
-  accountId: text().notNull(),
-  accessToken: text(),
-  refreshToken: text(),
-  accessTokenExpiresAt: timestamp({ mode: 'date', withTimezone: true }),
-  refreshTokenExpiresAt: timestamp({ mode: 'date', withTimezone: true }),
-  scope: text(),
-  password: text(),
-  createdAt: timestamp({ mode: 'date', withTimezone: true }).defaultNow().notNull(),
-  updatedAt: timestamp({ mode: 'date', withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
+  accountId: text('account_id').notNull(),
+  accessToken: text('access_token'),
+  refreshToken: text('refresh_token'),
+  accessTokenExpiresAt: timestamp('access_token_expires_at', { mode: 'date', withTimezone: true }),
+  refreshTokenExpiresAt: timestamp('refresh_token_expires_at', { mode: 'date', withTimezone: true }),
+  scope: text('scope'),
+  password: text('password'),
+  createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true }).notNull().defaultNow().$onUpdate(() => new Date()),
 });
 export type Account = typeof account.$inferSelect;
 export type NewAccount = typeof account.$inferInsert;
 
 export const verification = auth.table('verification', {
-  id: uuid().primaryKey().notNull().defaultRandom(),
-  identifier: text().notNull(),
-  value: text().notNull(),
-  expiresAt: timestamp({ mode: 'date', withTimezone: true }).notNull(),
-  createdAt: timestamp({ mode: 'date', withTimezone: true }).defaultNow().notNull(),
-  updatedAt: timestamp({ mode: 'date', withTimezone: true })
+  id: uuid('id').primaryKey().notNull().defaultRandom(),
+  identifier: text('identifier').notNull(),
+  value: text('value').notNull(),
+  expiresAt: timestamp('expires_at', { mode: 'date', withTimezone: true }).notNull(),
+  createdAt: timestamp('created_at', { mode: 'date', withTimezone: true }).defaultNow().notNull(),
+  updatedAt: timestamp('updated_at', { mode: 'date', withTimezone: true })
     .notNull()
     .defaultNow()
     .$onUpdate(() => new Date()),
