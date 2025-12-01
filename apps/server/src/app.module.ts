684ee5743f35a58c81c00cb6cb19bbc1fc4d9d7e
Ehren Castillo <ehrencastillo@gmail.com>
2025-12-01 13:10:33 +0800

    feat(apps/server): setup proper module imports

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,15 +1,38 @@
 import { AuthModule } from '@thallesp/nestjs-better-auth';
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { auth } from './auth';
 
+import { AddressesModule } from './addresses/addresses.module';
+import { AgenciesModule } from './agencies/agencies.module';
+import { BookingsModule } from './bookings/bookings.module';
 import { DatabaseModule } from './database/database.module';
+import { MessagesModule } from './messages/messages.module';
+import { PortfoliosModule } from './portfolios/portfolios.module';
+import { ProvidersModule } from './providers/providers.module';
+import { RequestsModule } from './requests/requests.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { SeekersModule } from './seekers/seekers.module';
+import { ServicesModule } from './services/services.module';
+import { ServiceTypesModule } from './service-types/service-types.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule.forRoot({ auth })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DatabaseModule,
+    AuthModule.forRoot({ auth }),
+    UsersModule,
+    ServicesModule,
+    ServiceTypesModule,
+    ProvidersModule,
+    BookingsModule,
+    ReviewsModule,
+    AddressesModule,
+    AgenciesModule,
+    SeekersModule,
+    RequestsModule,
+    PortfoliosModule,
+    MessagesModule,
+  ],
 })
 export class AppModule {}
